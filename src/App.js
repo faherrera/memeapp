17177ee960b2151a216e9512b7946121b328dc69
Facundo Herrera <faherrera.dev@gmail.com>
2019-05-21 22:29:22 -0300

    Create all the entire project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import CardSection from './containers/CardSection';
+import StorageService from './utils/localStorageService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const _instanceStorage = new StorageService(); //Instancia del storageService.
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      title: "",
+      img: "",
+      data: [],
+    }
+
+    this.handleOnChangeInput = this.handleOnChangeInput.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  componentDidMount(){
+    // En este ciclo de vida es el indicado para saber si tengo o no datos en el localStorage.
+    // Traer datos, condicionar, si tengo actualizar el estado.
+
+  }
+
+  handleOnChangeInput(event){
+    this.setState({
+      [event.target.name]: event.target.value      
+    })
+  }
+
+  handleOnClick(){
+    const {data, title,img} = this.state;
+    
+    const addData  = [...data, {
+      title,
+      img
+    }] ;
+
+    this.setState({
+      data: addData
+    })
+
+    //Aquí se debería setear el localStorage.
+  }
+
+  render(){
+    return(
+      <div className="wrapper">
+        <div className="container-header">
+          <Header title="MemeApp"/>
+          <nav className="nav-app">
+            <input type="text" onChange={this.handleOnChangeInput} placeholder="Ingrese un title" name="title" value={this.state.title}/>
+            <input type="text" onChange={this.handleOnChangeInput} placeholder="Ingrese una url" name="img" value={this.state.img}/>
+            <button onClick={this.handleOnClick}> Agregar </button>
+          </nav>
+        </div>
+
+        <CardSection data={ this.state.data }/>
+      </div>
+    )
+  }
 }
 
 export default App;
